0dc331f5973355314b733c6dd6fdd28dd94d4652
Carsten Kuckuk <ck@kuckuk.com>
2018-10-07 23:51:28 +0200

    'init' command now creates an empty tree as the initial commit

--- src/commands/InitCommand.cpp
+++ src/commands/InitCommand.cpp
@@ -34,16 +34,54 @@ static void WriteBackupnadoConfigurationFile(std::string strNodeName, std::strin
 	WriteToFile(".backupnado", BC);
 }
 
+static void CreateEmptyCommit(git_repository *pRepo);
+static void CreateBranchFromEmptyCommit(git_repository *pRepo, std::string strBranchName);
 static void InitializeGitRepositoryForBackupOfComputerAt(std::string strNodeName, std::string strSourceDirectory, std::string strTargetDirectory)
 {
 	git_repository *pRepo = 0;
 
 	git_repository_init_options opts;
 	git_repository_init_init_options(&opts, GIT_REPOSITORY_INIT_OPTIONS_VERSION);
+	opts.flags |= GIT_REPOSITORY_INIT_BARE | GIT_REPOSITORY_INIT_MKDIR | GIT_REPOSITORY_INIT_MKPATH;;
+	// Mit NO_REINIT arbeiten um zu testen, ob das Repo bereits existiert
 	opts.description = "backupnado repository";
 
 	int nRetVal = git_repository_init_ext(&pRepo, strTargetDirectory.c_str(), &opts);
 
+	CreateEmptyCommit(pRepo);
+	CreateBranchFromEmptyCommit(pRepo, strNodeName);
+
 	git_repository_free(pRepo);
 }
 
+static void CreateEmptyCommit(git_repository *pRepo)
+{
+	git_signature *sig=0;
+	int nRet1 = git_signature_now(&sig, "Backupnado", "backupnado@backupnado.net");
+
+	git_index *index=0;
+	int nRet2 = git_repository_index(&index, pRepo);
+
+
+	git_oid tree_id;
+	int nRet3 = git_index_write_tree(&tree_id, index);
+	git_index_free(index);
+
+	git_tree *tree = 0;
+	int nRet4 = git_tree_lookup(&tree, pRepo, &tree_id);
+
+	git_oid commit_id;
+	int nRet5 = git_commit_create_v(
+			&commit_id, pRepo, "HEAD", sig, sig,
+		NULL, 
+		"This is the initial commit of a Backupnado repository. This repository contains computer backups "
+		"in its branches. For further information please read through the backupnado documentation.", tree, 0);
+	
+	git_tree_free(tree);
+	git_signature_free(sig);
+}
+
+static void CreateBranchFromEmptyCommit(git_repository *pRepo, std::string strBranchName)
+{
+
+}
